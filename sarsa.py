ab7a4ea246742d6659a9f28f0cdd87d272b9260d
davetropeano <davetropeano@us.ibm.com>
2020-01-09 20:05:36 -0500

    initial - check shape of Q

--- sarsa.py
+++ sarsa.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time, pickle, os
+import matplotlib.pyplot as pyplot
 
 class ObservationSpace:
     def __init__(self, n):
@@ -22,10 +23,17 @@ class ActionSpace:
 class Opponent:
     def __init__(self):
         self.moves = []
+        self.last_move = 1
 
     def get_move(self):
+        """
         eps = np.random.uniform()
         move = 1 if eps > 0.5 else 0
+        """
+
+        # alternate
+        move = 0 if self.last_move == 1 else 1
+        self.last_move = move
         self.moves.append(move)
         return move
 
@@ -33,7 +41,7 @@ class PrisonersDilemma:
     def __init__(self, moves_in_memory=5, max_score=500):
         super().__init__()
         self.max_score = max_score
-        self.me_score = self.you_score = 0
+        self.my_score = self.their_score = 0
         self.move = 0
 
         self.opponent = Opponent()
@@ -64,14 +72,13 @@ class PrisonersDilemma:
         ]
 
     def reset(self):
-        self.my_score = self.you_score = 0
+        self.my_score = self.their_score = 0
         self.move = 0
         self.observation_space.reset()
         return self._get_obs()
 
     def render(self):
-        print('---------------------')
-        print('me: %s, you: %s, memory: %s' % (self.me_score, self.you_score, self._get_obs()))
+        print('me: %s, them: %s, memory: %s' % (self.my_score, self.their_score, self._get_obs()))
 
     def _get_obs(self):
         return self.observation_space.space
@@ -89,15 +96,15 @@ class PrisonersDilemma:
 
     def step(self, action):
         # assume always an allowed action
-        you_action = self.opponent.get_move()
-        payoffs = self.reward_table[action][you_action]
-        self.me_score += payoffs[0]
-        self.you_score += payoffs[1]
+        their_action = self.opponent.get_move()
+        payoffs = self.reward_table[action][their_action]
+        self.my_score += payoffs[0]
+        self.their_score += payoffs[1]
         
-        done = True if self.me_score >= self.max_score or self.you_score >= self.max_score else False
+        done = True if self.my_score >= self.max_score or self.their_score >= self.max_score else False
         reward = payoffs[0]
 
-        self._update_observation(you_action)
+        self._update_observation(their_action)
         self.move += 1
 
         return self._get_obs(), reward, done, None
@@ -105,12 +112,9 @@ class PrisonersDilemma:
 
 env = PrisonersDilemma()
 
-epsilon = 0.9
-# min_epsilon = 0.1
-# max_epsilon = 1.0
-# decay_rate = 0.01
-
-total_episodes = 10000
+epsilon = 1.0
+total_episodes = 100
+max_steps = 300
 
 lr_rate = 0.81
 gamma = 0.96
@@ -119,7 +123,7 @@ Q = np.zeros((2**env.observation_space.n, env.action_space.n))
     
 def choose_action(state):
     action=0
-    if np.random.uniform(0, 1) < epsilon:
+    if np.random.random() >= 1-epsilon:
         action = env.action_space.sample()
     else:
         action = np.argmax(Q[state, :])
@@ -141,6 +145,7 @@ if __name__ == "__main__":
     rewards = []
 
     for episode in range(total_episodes):
+        print(epsilon)
         t = 0
         reward_total = 0
 
@@ -148,15 +153,11 @@ if __name__ == "__main__":
         state = observation_to_state(obs)
         action = choose_action(state)
 
-        # Setting max steps per game (episode) to a constant drives to a defect-only strategy
-        # by induction. Instead, don't let the AI train only a fixed number of steps and instead vary this
-        max_steps = np.random.randint(100, 1000)    
-
-
         done = False
         while not done:
             obs, reward, done, info = env.step(action)
             state2 = observation_to_state(obs)
+            reward_total += reward
 
             action2 = choose_action(state2)
 
@@ -166,17 +167,18 @@ if __name__ == "__main__":
             action = action2
 
             t += 1
-            reward_total += reward
+            if t == max_steps:
+                done = True
 
         rewards.append(reward_total)
-        env.render()
+        epsilon -= 2/(total_episodes) if epsilon > 0 else 0
 
-    # epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode) 
-    # os.system('clear')
+        #env.render()
 
-        
+    print(rewards)
     print ("Score over time: ", sum(rewards)/total_episodes)
-    print(Q)
+    # print(Q)
+    pyplot.plot(rewards, 'b--')
 
     with open("pd_qTable_sarsa.pkl", 'wb') as f:
         pickle.dump(Q, f)
